46cdfc03646b15c80468a83741752f254c4acbcc
Wina Zakaria <winazakaria0@gmail.com>
2024-01-02 10:20:13 +0100

    Add NotFound Page

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,9 @@
 // Router
 import router from "./router/router.js";
 
+// Pages
+import NotfoundPage from "./pages/NotfoundPage.js";
+
 window.addEventListener("DOMContentLoaded", () => {
   router.init();
 });
---
+++ frontend/pages/NotfoundPage.js
@@ -0,0 +1,12 @@
+export default class NotfoundPage extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    const template = document.getElementById("notfound-template");
+    const component = template.content.cloneNode(true);
+    this.appendChild(component);
+  }
+}
+
+customElements.define("notfound-page", NotfoundPage);
