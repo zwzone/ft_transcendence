956ada53c45c3a3c500875da75277acc09f23a1c
Wina Zakaria <winazakaria0@gmail.com>
2024-01-09 12:07:23 +0100

    Tweek Auth app

--- backend/authenticationapp/views.py
+++ backend/authenticationapp/views.py
@@ -1,19 +1,13 @@
-import os
-
 import requests
-from django.http import JsonResponse
-from dotenv import load_dotenv
+from os import getenv
 from django.contrib.auth import login, get_user_model
 from django.shortcuts import redirect
 from django.urls import reverse
 from rest_framework import serializers
 from rest_framework import status
 from rest_framework.response import Response
-
 from rest_framework.views import APIView
 from .models import Player
-
-load_dotenv()
 
 
 class PlayerSerializer(serializers.Serializer):
@@ -27,13 +21,9 @@ class PublicApi(APIView):
 
 
 class IntraRedirectAPIView(PublicApi):
-
     def get(self, request, *args, **kwargs):
-        redirect_uri = request.build_absolute_uri(reverse('authAPI'))
-        authorization_url = (
-            f"https://api.intra.42.fr/oauth/authorize?client_id={os.environ.get('CLIENT_ID')}"
-            f"&redirect_uri={redirect_uri}&response_type=code"
-        )
+        redirect_uri = request.build_absolute_uri(reverse("authAPI"))
+        authorization_url = f"https://api.intra.42.fr/oauth/authorize?client_id={getenv('CLIENT_ID')}&redirect_uri={redirect_uri}&response_type=code"
         return redirect(authorization_url)
 
 
@@ -42,82 +32,70 @@ class IntraAuthAPIView(PublicApi):
         code = serializers.CharField(required=False)
 
     def get(self, request, *args, **kwargs):
-        redirect_uri = request.build_absolute_uri(reverse('authAPI'))
+        redirect_uri = request.build_absolute_uri(reverse("authAPI"))
         input_serializer = self.InputSerializer(data=request.GET)
         input_serializer.is_valid(raise_exception=True)
-
         validated_data = input_serializer.validated_data
         code = validated_data.get("code")
         error_message = validated_data.get("error")
-
         if error_message is not None:
-            return JsonResponse(
-                dict(statusCode=401, error=error_message),
-                status=status.HTTP_200_OK
+            return Response(
+                dict(statusCode=401, error=error_message), status=status.HTTP_200_OK
             )
-
         if code is None:
-            return JsonResponse(
-                {"statusCode": status.HTTP_401_UNAUTHORIZED}
-            )
-
+            return Response({"statusCode": status.HTTP_401_UNAUTHORIZED})
         if request.user.is_authenticated:
-            return JsonResponse(
+            return Response(
                 {"statusCode": status.HTTP_200_OK, "Already logged in": error_message}
             )
-
-        if request.method == "GET":
-            code = request.GET.get("code")
-
-            if code:
-                data = {
-                    "grant_type": "authorization_code",
-                    "client_id": os.environ.get("CLIENT_ID"),
-                    "client_secret": os.environ.get("CLIENT_SECRET"),
-                    "code": code,
-                    "redirect_uri": redirect_uri,
+        code: str = request.GET.get("code")
+        if code:
+            return Response(
+                {"statusCode": 200, "Already logged in": error_message},
+                status=status.HTTP_200_OK,
+            )
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": getenv("CLIENT_ID"),
+            "client_secret": getenv("CLIENT_SECRET"),
+            "code": code,
+            "redirect_uri": redirect_uri,
+        }
+        auth_response = requests.post(
+            "https://api.intra.42.fr/oauth/token",
+            data=data,
+        )
+        if "error_description" in auth_response.json():
+            return Response(
+                {
+                    "statusCode": status.HTTP_401_UNAUTHORIZED,
+                    "detail": auth_response.json().get("error_description"),
+                },
+                status=status.HTTP_200_OK,
+            )
+        access_token = auth_response.json()["access_token"]
+        user_response = requests.get(
+            "https://api.intra.42.fr/v2/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        if not user_response or user_response.status_code != 200:
+            return Response(
+                {
+                    "statusCode": status.HTTP_403_FORBIDDEN,
+                    "detail": "No access token in the token response",
                 }
-                auth_response = requests.post(
-                    "https://api.intra.42.fr/oauth/token",
-                    data=data, )
-
-                if 'error_description' in auth_response.json():
-                    return JsonResponse(
-                        {"statusCode": status.HTTP_401_UNAUTHORIZED,
-                         "detail": auth_response.json().get("error_description")},
-                        status=status.HTTP_200_OK
-                    )
-
-                access_token = auth_response.json()["access_token"]
-                user_response = requests.get("https://api.intra.42.fr/v2/me"
-                                             , headers={"Authorization": f"Bearer {access_token}"})
-
-                if not user_response or user_response.status_code != 200:
-                    return JsonResponse(
-                        {"statusCode": status.HTTP_403_FORBIDDEN, "detail": "No access token in the token response"}
-                    )
-
-                username = user_response.json()["login"]
-                display_name = user_response.json()["displayname"]
-                email = user_response.json()["email"]
-
-                try:
-                    # print(Player.objects.get(username=username))
-                    # user = Player.objects.get(username=username)
-                    user = get_user_model().objects.get(username=username)
-                    return JsonResponse(
-                        {"statusCode": 200},
-                        status=status.HTTP_200_OK
-                    )
-                except Player.DoesNotExist:
-                    user = Player.objects.create_user(username=username, display_name=display_name, email=email)
-                    login(request, user)
-                    return JsonResponse(
-                        {"statusCode": 200},
-                        status=status.HTTP_200_OK
-                    )
-            else:
-                return JsonResponse(
-                    {"statusCode": 200, "Already logged in": error_message},
-                    status=status.HTTP_200_OK
-                )
+            )
+        username = user_response.json()["login"]
+        display_name = user_response.json()["displayname"]
+        email = user_response.json()["email"]
+        try:
+            # print(Player.objects.get(username=username))
+            # user = Player.objects.get(username=username)
+            user = get_user_model().objects.get(username=username)
+            return Response({"statusCode": 200}, status=status.HTTP_200_OK)
+        except Player.DoesNotExist:
+            user = Player.objects.create_user(
+                username=username, display_name=display_name, email=email
+            )
+            login(request, user)
+            return Response({"statusCode": 200}, status=status.HTTP_200_OK)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv, path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BACKEND_DIR = Path(__file__).resolve().parent.parent
+FT_TRANSCENDENCE_DIR = Path(__file__).resolve().parent.parent.parent
 
+# Load .env file
+dotenv_file = FT_TRANSCENDENCE_DIR / ".env"
+if path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lv$8%4blf=scn7h4i_s%-7vl7u=f*zvaz)$0!=g#@ods&h0!_a'
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,56 +38,56 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'authenticationapp',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "authenticationapp.apps.AuthenticationappConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
-AUTH_USER_MODEL = 'authenticationapp.Player'
+AUTH_USER_MODEL = "authenticationapp.Player"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BACKEND_DIR / "db.sqlite3",
     }
 }
 
@@ -90,16 +97,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -107,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Africa/Casablanca'
+TIME_ZONE = "Africa/Casablanca"
 
 USE_I18N = True
 
@@ -119,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
