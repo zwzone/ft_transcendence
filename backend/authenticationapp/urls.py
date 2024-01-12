58e6baee9b0750e745e6963642e1f44385746126
Mostapha ID <mostapha@ReoGines-MacBook-Air.local>
2024-01-12 09:59:06 +0100

    Finish Intra auth and Google auth

--- backend/authenticationapp/urls.py
+++ backend/authenticationapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from authenticationapp.views import intra_auth, callback_auth
+from authenticationapp.views import intra_auth, intra_callback_auth, google_auth, google_callback_auth
 
 urlpatterns = [
-    path('intra/', intra_auth, name='intraAPI'),
-    path('callback/', callback_auth, name='callbackAPI'),
+    path('intra/', intra_auth, name='intraView'),
+    path('intracallback/', intra_callback_auth, name='intracallbackView'),
+    path('google/', google_auth, name='googleView'),
+    path('googlecallback/', google_callback_auth, name='googlecallbackView')
 ]
--- backend/authenticationapp/views.py
+++ backend/authenticationapp/views.py
@@ -1,48 +1,88 @@
 import requests
 from os import getenv
-from django.contrib.auth import login, get_user_model
 from django.shortcuts import redirect, reverse
 from django.utils.http import urlencode
-from rest_framework import status
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Player
+# from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
 def intra_auth(request):
-    redirect_uri = urlencode({"redirect_uri": request.build_absolute_uri(reverse("callbackAPI"))})
-    authorization_url = f"https://api.intra.42.fr/oauth/authorize?client_id={getenv('CLIENT_ID')}&{redirect_uri}&response_type=code"
+    redirect_uri = urlencode({"redirect_uri": request.build_absolute_uri(reverse("intracallbackView"))})
+    authorization_url = f"https://api.intra.42.fr/oauth/authorize?client_id={getenv('INTRA_CLIENT_ID')}&{redirect_uri}&response_type=code"
     return redirect(authorization_url)
 
 
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
-def callback_auth(request):
+def intra_callback_auth(request):
     code = request.GET.get("code")
     error_message = request.GET.get("error")
     if error_message is not None:
-        return Response({"statusCode": 401, "error": error_message}, status=status.HTTP_200_OK)
+        return Response({"statusCode": 401, "error": error_message})
     if code is None:
-        return Response({"statusCode": status.HTTP_401_UNAUTHORIZED})
+        return Response({"statusCode": 401, "error": "code is required"})
     if request.user.is_authenticated:
-        return Response({"statusCode": status.HTTP_200_OK, "Already logged in": error_message})
+        return Response({"statusCode": 200, "message": "Already logged in"})
     data = {
         "grant_type": "authorization_code",
-        "client_id": getenv("CLIENT_ID"),
-        "client_secret": getenv("CLIENT_SECRET"),
+        "client_id": getenv("INTRA_CLIENT_ID"),
+        "client_secret": getenv("INTRA_CLIENT_SECRET"),
         "code": code,
-        "redirect_uri": request.build_absolute_uri(reverse("callbackAPI")),
+        "redirect_uri": request.build_absolute_uri(reverse("intracallbackView")),
     }
     auth_response = requests.post("https://api.intra.42.fr/oauth/token", data=data)
-    if "error_description" in auth_response.json():
-        return Response({"statusCode": status.HTTP_401_UNAUTHORIZED, "detail": auth_response.get("error_description")},
-                        status=status.HTTP_200_OK)
-    response = redirect("http://localhost/home")
-    response.set_cookie("access_token", auth_response.json()["access_token"])
-    response.set_cookie("refresh_token", auth_response.json()["refresh_token"])
-    return response
+    if not auth_response.ok:
+        return Response({"statusCode": 401, "detail": auth_response.get("error_description")})
+    # TODO: JWT
+    return Response({"statusCode": 200, "JWT": ""})
+
+@api_view(["GET"])
+@authentication_classes([])
+@permission_classes([])
+def google_auth(request):
+    SCOPES = [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+    ]
+    params = {
+        "response_type": "code",
+        "client_id": getenv("GOOGLE_CLIENT_ID"),
+        "redirect_uri": request.build_absolute_uri(reverse("googlecallbackView")),
+        "scope": " ".join(SCOPES),
+        "access_type": "offline",
+        "include_granted_scopes": "true",
+        "prompt": "select_account",
+    }
+    query_params = urlencode(params)
+    GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/auth"
+    google_authorization_url = f"{GOOGLE_AUTH_URL}?{query_params}"
+    return redirect(google_authorization_url)
+
+@api_view(["GET"])
+@authentication_classes([])
+@permission_classes([])
+def google_callback_auth(request):
+    code = request.GET.get("code")
+    error = request.GET.get("error")
+    if error is not None:
+        return Response({"statusCode": 200, "error": error})
+    if code is None:
+        return Response({"statusCode": 200, "error": "User Not Autorized"})
+    data = {
+        "code": code,
+        "client_id": getenv("GOOGLE_CLIENT_ID"),
+        "client_secret": getenv("GOOGLE_CLIENT_SECRET"),
+        "redirect_uri": request.build_absolute_uri(reverse("googlecallbackView")),
+        "grant_type": "authorization_code",
+    }
+    auth_response = requests.post("https://oauth2.googleapis.com/token", data=data)
+    if not auth_response.ok :
+        return Response({"statusCode": 200, "error": "Failed to obtain access token from Google."})
+    # TODO: JWT
+    return Response({"statusCode": 200, "JWT": ""})
