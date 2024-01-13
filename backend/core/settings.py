692f923282f879cc1ccda129d50c54671851f50c
GowthertG <houssamhaddouche777@gmail.com>
2024-01-13 16:18:13 +0100

    Setup API application Django; Add Swagger

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
     "authenticationapp.apps.AuthenticationappConfig",
     "apiapp.apps.ApiappConfig",
 ]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,10 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your Project API",
+      default_version='v1',
+      description="API documentation for your project",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authenticationapp.urls')),
     path('api/', include('apiapp.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
