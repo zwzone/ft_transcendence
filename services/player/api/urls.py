cc3510aa9ba72e79537df1938ef227cc6fbf424b
Mostapha ID <midfath@student.1337.ma>
2024-01-21 11:07:32 +0100

    REFACTOR: create a endpoints.py file in player app

--- services/player/api/urls.py
+++ services/player/api/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import PlayerUsernameView, PlayerLastNameView, \
-    PlayerFirstNameView , PlayerInfoView , PlayerAvatarView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,11 +31,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('player/', PlayerInfoView.as_view(), name='playerView'),
-    path('player/username', PlayerUsernameView.as_view(), name='playerUsernameView'),
-    path('player/first_name', PlayerFirstNameView.as_view(), name='playerFirstNameView'),
-    path('player/last_name', PlayerLastNameView.as_view(), name='playerLastNameView'),
-    path('player/avatar', PlayerAvatarView.as_view(), name='playerAvatarView'),
+    path('player/', include('.endpoints'), name='playerEndpoints'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
