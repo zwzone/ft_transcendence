7d08d102ead44fb914b1207b02a74ae1305ac115
Mostapha ID <midfath@student.1337.ma>
2024-01-29 10:20:24 +0100

    FIX: add trailing slash to the endpoints

--- services/authentication/api/middleware.py
+++ services/authentication/api/middleware.py
@@ -2,21 +2,13 @@ from django.core.cache import cache
 from rest_framework.response import Response
 
 
-def extract_jwt_from_request(request):
-    if not 'jwt_token' in request.COOKIES:
-        jwt_token = request.COOKIES['jwt_token']
-        return jwt_token
-    return
-
-
 class JWTRevocationMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        jwt_token = extract_jwt_from_request(request)
-        if jwt_token:
-            if cache.get(jwt_token) is not None:
-                return Response({"statusCode": 401, "error": "Invalid token"})
+        jwt_token = request.COOKIES['jwt_token']
+        if jwt_token and cache.get(jwt_token) is not None:
+            return Response({"statusCode": 401, "error": "Invalid token"})
         response = self.get_response(request)
         return response
--- services/authentication/api/urls.py
+++ services/authentication/api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.endpoints'), name='authEndpoints'),
+    path('authentication/', include('api.endpoints'), name='authEndpoints'),
 ]
