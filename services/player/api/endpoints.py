cc7db895a7a025b25b789ae4596f74e2fcdb85b1
GowthertG <houssamhaddouche777@gmail.com>
2024-01-30 15:24:22 +0100

    FIX: fix enum of player app models

--- services/player/api/endpoints.py
+++ services/player/api/endpoints.py
@@ -1,15 +1,8 @@
 from django.urls import path
-from .views import (
-    PlayerUsernameView,
-    PlayerLastNameView,
-    PlayerFirstNameView,
-    PlayerInfoView,
-    PlayerAvatarView,
-    PlayerAddFriend,
-)
-
+from . import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,12 +14,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', PlayerInfoView.as_view(), name='playerView'),
-    path('username/', PlayerUsernameView.as_view(), name='playerUsernameView'),
-    path('first_name/', PlayerFirstNameView.as_view(), name='playerFirstNameView'),
-    path('last_name/', PlayerLastNameView.as_view(), name='playerLastNameView'),
-    path('avatar/', PlayerAvatarView.as_view(), name='playerAvatarView'),
-    path('add_friend/', PlayerAddFriend.as_view(), name='playerAddFriendView'),
+    path('', views.PlayerInfoView.as_view(), name='playerView'),
+    path('username/', views.PlayerUsernameView.as_view(), name='playerUsernameView'),
+    path('first_name/', views.PlayerFirstNameView.as_view(), name='playerFirstNameView'),
+    path('last_name/', views.PlayerLastNameView.as_view(), name='playerLastNameView'),
+    path('avatar/', views.PlayerAvatarView.as_view(), name='playerAvatarView'),
+    path('add_friend/', views.PlayerAddFriend.as_view(), name='playerAddFriendView'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- services/player/api/models.py
+++ services/player/api/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from enum import Enum
 
 
+class FriendshipStatus(Enum):
+    ACCEPTED = 'accepted'
+    PENDING = 'pending'
+
+
 class Player(AbstractUser):
     email = models.EmailField(max_length=30, blank=False, null=False)
     first_name = models.CharField(max_length=20, blank=False, null=False)
@@ -16,23 +21,16 @@ class Player(AbstractUser):
         return self.email
 
 
-
 class Friendship(models.Model):
     STATUS_CHOICES = [
-        (FriendshipStatus.ACCEPTED.value, 'Accepted'),
-        (FriendshipStatus.PENDING.value, 'Pending'),
+        (FriendshipStatus.ACCEPTED.value, 'AC'),
+        (FriendshipStatus.PENDING.value, 'PN'),
     ]
 
     sender = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='sent_friend_requests')
     receiver = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='received_friend_requests')
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=FriendshipStatus.PENDING.value)
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=FriendshipStatus.PENDING.value)
 
     def __str__(self):
         return f'{self.sender.username} -> {self.receiver.username}'
-
-# Here we define any custom enums we want to use
-
-class FriendshipStatus(Enum):
-    ACCEPTED = 'accepted'
-    PENDING = 'pending'
