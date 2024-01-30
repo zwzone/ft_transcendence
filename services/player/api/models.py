9e07e369ae0bc27ab254623bf9bb23848147ee04
houssam eddine haddouche <houssamhaddouche777@gmail.com>
2024-01-30 12:05:44 +0100

    FIX: making enum for friendship status

--- services/player/api/models.py
+++ services/player/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from enum import Enum
 
 
 class Player(AbstractUser):
@@ -15,12 +16,23 @@ class Player(AbstractUser):
         return self.email
 
 
+
 class Friendship(models.Model):
-    sender = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='sent_friend_requests')
-    receiver = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='received_friend_requests')
+    STATUS_CHOICES = [
+        (FriendshipStatus.ACCEPTED.value, 'Accepted'),
+        (FriendshipStatus.PENDING.value, 'Pending'),
+    ]
+
+    sender = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='sent_friend_requests')
+    receiver = models.ForeignKey('Player', on_delete=models.CASCADE, related_name='received_friend_requests')
     created_at = models.DateTimeField(auto_now_add=True)
-    accepted = models.BooleanField(default=False)
-    pending = models.BooleanField(default=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=FriendshipStatus.PENDING.value)
 
     def __str__(self):
         return f'{self.sender.username} -> {self.receiver.username}'
+
+# Here we define any custom enums we want to use
+
+class FriendshipStatus(Enum):
+    ACCEPTED = 'accepted'
+    PENDING = 'pending'
