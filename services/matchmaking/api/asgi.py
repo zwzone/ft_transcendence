f7f895fd6d93d4a36bf84f5aeb91a61e9ec14b33
tnaceur <tnaceur@student.1337.ma>
2024-01-31 10:37:44 +0100

    REFACTOR: Make the matchmaking django project as single app

--- services/matchmaking/api/asgi.py
+++ services/matchmaking/api/asgi.py
@@ -4,7 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from main.routing import websocket_urlpatterns
+from .routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
--- services/matchmaking/api/settings.py
+++ services/matchmaking/api/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'daphne',
     'channels',
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
