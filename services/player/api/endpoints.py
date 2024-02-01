6be1558a655c83ae51a04f645cabe2131bad4b89
GowthertG <houssamhaddouche777@gmail.com>
2024-02-01 14:54:34 +0100

    REFACTOR: removing swagger

--- services/player/api/endpoints.py
+++ services/player/api/endpoints.py
@@ -1,17 +1,5 @@
 from django.urls import path
 from . import views
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Player API Documentation",
-        default_version='v1',
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('', views.PlayerInfoView.as_view(), name='playerView'),
