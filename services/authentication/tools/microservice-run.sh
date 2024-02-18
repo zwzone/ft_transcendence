e02e0e24b422f1f25042c575ea4812359a1a2259
Zakaria Wina <winazakaria0@gmail.com>
2024-02-18 13:28:25 +0100

    FIX: add collectstatic

--- services/authentication/tools/microservice-run.sh
+++ services/authentication/tools/microservice-run.sh
@@ -4,6 +4,8 @@ python manage.py makemigrations
 
 python manage.py migrate --run-syncdb
 
+python manage.py collectstatic --noinput
+
 if [[ $STAGE == "Deployment" ]]
 then
     daphne api.asgi:application --port 8000 --bind 0.0.0.0 -v2
