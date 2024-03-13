1c422f21ebcca5817fb24f6ca84217dc8f8fb42e
Zakaria Wina <winazakaria0@gmail.com>
2024-03-13 09:29:37 +0000

    ADD: changing color palette with button in Frontend

--- client/js/components/Navbar.js
+++ client/js/components/Navbar.js
@@ -24,10 +24,25 @@ export default class Navbar extends HTMLElement {
     };
 
     const logo = this.querySelector(".logo");
+    const colorizer = this.querySelector(".colorizer.btn");
     const links = this.querySelectorAll(".link.btn");
     const hamburger = this.querySelector(".hamburger");
 
     logo.addEventListener("click", handleLink);
+
+    colorizer.addEventListener("click", (event) => {
+      const color_primary = document.documentElement.style.getPropertyValue("--color-primary");
+      if (!color_primary || color_primary === "#f8ec9030") {
+        document.documentElement.style.setProperty("--color-primary", "#2cacff30");
+        document.documentElement.style.setProperty("--color-primary-solid", "#2e2c1d");
+        document.documentElement.style.setProperty("--color-primary-light", "#2cacff");
+      } else if (color_primary === "#2cacff30") {
+        document.documentElement.style.setProperty("--color-primary", "#f8ec9030");
+        document.documentElement.style.setProperty("--color-primary-solid", "#f8ec90");
+        document.documentElement.style.setProperty("--color-primary-light", "#f8ec90");
+      }
+    });
+
     links.forEach((link) => {
       link.addEventListener("click", handleLink);
     });
