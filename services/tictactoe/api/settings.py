b94ac803f5826b7ed9169fef04406d2d8747fbd0
Zakaria Wina <winazakaria0@gmail.com>
2024-03-15 00:40:54 +0000

    FIX: pong duplications and tictactoe debug variable

--- services/tictactoe/api/settings.py
+++ services/tictactoe/api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("STAGE") == "Development"
 
 ALLOWED_HOSTS = []
 
